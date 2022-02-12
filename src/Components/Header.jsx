b227148623d51d7907f4460d1d8c1a7b536d31cf
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-02-12 13:02:13 -0300

    um pouco de css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,15 +4,17 @@ import { Link } from 'react-router-dom';
 class Header extends React.Component {
   render() {
     return (
-      <nav>
-        <div>
-          <Link to="/">Hi its me</Link>
+      <header>
+        <nav>
           <ul>
             <li><Link to="/">Inicio</Link></li>
             <li><Link to="/new">Adicionar</Link></li>
           </ul>
-        </div>
-      </nav>
+          <div id="logo">
+            <Link to="/">Hi its me</Link>
+          </div>
+        </nav>
+      </header>
     );
   }
 }
