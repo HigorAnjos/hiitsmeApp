6c10031d11a9ec88abf60456a9a7d74097360e18
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-02-13 21:29:15 -0300

    funcao makeAllCards

--- src/Components/Asidecards.jsx
+++ src/Components/Asidecards.jsx
@@ -2,10 +2,30 @@ import React from 'react';
 import Card from './card/card';
 
 class Asidecards extends React.Component {
+  makeAllCards = () => {
+    const { storageApp } = this.props;
+    const acc = [];
+    storageApp.forEach(element => {
+      element.data.forEach((e) => {
+        acc.push(
+          <Card
+            key={ e.id }
+            category={ element.category }
+            done={ e.done }
+            reaction={ e.reaction }
+            choice={ e.choice }
+            date={ e.date }
+          />
+        );
+      });
+    });
+    return acc;
+  }
+  
   render() {
     return(
       <div id="aside">
-        <Card />
+        { this.makeAllCards() }
       </div>
     );
   }
--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Asidecards from '../Components/Asidecards';
 import CardView from '../Components/Mainview/CardView';
+import { getAll } from '../services/API';
 
 class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      storageApp: [],
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      storageApp: getAll(),
+    });
+  }
+
+
   render() {
     return (
       <div id="main">
-        <Asidecards />
+        <Asidecards { ...this.state } />
         <CardView />
       </div>
     );
